aa0bc7cc3e4fea5ea43d61718305099b06f613d9
EdoardoRaimondoSenis <166326043+EdoardoRaimondoSenis@users.noreply.github.com>
2024-06-03 17:30:53 +0200

    esercizio completato (mi sono dimenticato di committare) :((

---
+++ main.js
@@ -0,0 +1,9 @@
+let nome = prompt("inserisci il tuo nome");
+let cognome = prompt("inserisci il tuo cognome");
+let cp = prompt("inserisci il tuo colore preferito");
+
+let risultato = nome + cognome + cp + 23;
+
+console.log("La password creata è: " + risultato);
+
+document.write(risultato).innerHTML;
